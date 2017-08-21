22520d18b7fd9a2f4a710bf96bc4a680cc4a5333
maydinunlu <maydinunlu@gmail.com>
2017-08-21 11:54:27 +0300

    GPSManager

---
+++ GPS/Assets/Scripts/GPSManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class GPSManager : MonoBehaviour {
+
+    #region Singleton Instance
+
+    public static GPSManager Instance { get; set; }
+
+    #endregion
+
+    public float Latitude;  //Enlem
+    public float Longitude; // Boylam
+
+    /**************************************************/
+
+    #region Start
+
+    void Start() {
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        StartCoroutine(StartLocationService());
+    }
+
+    #endregion
+
+    #region LocationService: Start
+
+    private IEnumerator StartLocationService() {
+        if (!Input.location.isEnabledByUser) {
+            Debug.LogWarning("GPS is not active!");
+            yield break;
+        }
+
+        Input.location.Start();
+        int maxWait = 20;
+        bool wait = (Input.location.status == LocationServiceStatus.Initializing && maxWait > 20);
+        while (wait) {
+            yield return new WaitForSeconds(1f);
+            maxWait--;
+        }
+
+        if (maxWait <= 0) {
+            Debug.LogWarning("Timed Out");
+            yield break;
+        }
+
+        if (Input.location.status == LocationServiceStatus.Failed) {
+            Debug.LogWarning("Unable to determine device location");
+            yield break;
+        }
+
+        Latitude = Input.location.lastData.latitude;
+        Longitude = Input.location.lastData.longitude;
+
+    }
+
+    #endregion
+
+}
---
+++ GPS/Assets/Scripts/GPSView.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GPSView : MonoBehaviour {
+
+    public Text TxtLatitude;
+    public Text TxtLongitude;
+
+    /**************************************************/
+
+    #region Update
+
+    private void Update() {
+        TxtLatitude.text  = "Latitude: " + GPSManager.Instance.Latitude.ToString();
+        TxtLongitude.text = "Longitude: " +  GPSManager.Instance.Longitude.ToString();
+    }
+
+    #endregion
+
+}
